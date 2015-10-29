0f8c56879089ea1ecf76372accfd52e6ef428c29
ToruNiina <niina.toru.68u@gmail.com>
2015-10-29 18:31:49 +0900

    forgot to add new file

---
+++ includes/PDBdef.hpp
@@ -0,0 +1,13 @@
+#ifndef ARABICA_DEF
+#define ARABICA_DEF
+
+namespace arabica
+{
+    enum MOLECULE_TYPE
+    {
+        DNA,
+        PROTEIN,
+        UNKNOWN
+    };//TODO: add RNA
+}
+#endif//ARABICA_DEF
